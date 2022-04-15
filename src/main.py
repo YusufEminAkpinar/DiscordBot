62ac43b5e8344d38b9427c173491aabade7c2223
ScatteredCarrion <yakpinar137@gmail.com>
2022-04-15 14:50:35 +0300

    Adding some extra suggestions.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import random
 from time import sleep
 from reddit import red_post, secret_red_post
 import sys
-sys.path.insert(0, 'C:\\Users\\Hardal\\PycharmProjects\\dc_bot\\Others')
+sys.path.insert(0, '..\\Others')
 from cred import SECRET
 
 client = discord.Client()
@@ -16,28 +16,49 @@ def percent(x):
         return False
 
 
+shrek_ascii = '''
+⠑⡄⠀⠀⠀⠀⠀⠀ ⠀ ⣀⣀⣤⣤⣤⣀⡀
+⠸⠿⡀⠀ ⠀ ⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠸⣼⡿
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇
+⠀⠀ ⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠇
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+'''
+
+gif_trigger = {'sinüs', 'sin', 'cos', 'fizikçi', 'fizikçiler', 'kanit', 'kosinüs', 'cosinus', 'kanıt', 'sinus'}
+
 @client.event
 async def on_ready():
+    global tilt_etme
     print('We have logged in as {}'.format(client.user))
+    tilt_etme = False
+    print(f'Tilt etme:{tilt_etme}')
 
 @client.event
 async def on_message(message):
+    global tilt_etme
     if message.author == client.user:
         return
 
+    message_list = message.content.lower().split()
 
-    # BULLY QQN
-    tilt_etme = False
-    if message.content.lower() == '!kanseraç':
-        tilt_etme = True
-        await message.channel.send('Kanser açık :)')
-    if message.content.lower() == '!kanserkapat':
-        tilt_etme = False
-    if message.author.id == 954666083544096838:
-        if tilt_etme:
-            await message.channel.send('WOMEN MOMENT!!!!')
-            await message.channel.send('https://www.youtube.com/watch?v=e9mVfv3b-4E')
+    if message.content.startswith('!öneri'):
+        suggestion = str(message.content.split(' ')[1:])
+        with open('..\\Others\\yapılacaklar.txt', 'a') as file:
+            file.write(f'{suggestion}\n')
+        await message.channel.send(f'Öneriniz alındı. {suggestion} listeye eklendi :).')
 
+    if message.content.startswith('!yapılacaklar'):
+        with open('..\\Others\\yapılacaklar.txt', 'r') as file:
+            yapilacaklar = file.read()
+        await message.channel.send(f'Yapılacaklar listesi:\n{yapilacaklar}')
 
     # Women Moment !!!
     if message.content.lower() == '!women':
@@ -45,24 +66,46 @@ async def on_message(message):
         await message.channel.send('https://www.youtube.com/watch?v=e9mVfv3b-4E')
 
 
+    if 'shrek' in message_list:
+        await message.channel.send(shrek_ascii)
+
+
+    if 'alper' in message_list:
+        with open('..\\img\\gigachad.jpg', 'rb') as fh:
+            f = discord.File(fh, filename='gigachad.jpg')
+        await message.channel.send('Alper abin burda kardeşim', file=f)
+
+
+    if set(message_list) & gif_trigger != set():
+        with open('..\\img\\optimized_türev.gif', 'rb') as fh:
+            f = discord.File(fh, filename='iki_kere_türevini_alıyorsun.gif')
+        await message.channel.send(file=f)
+
+
     # tebip
-    if 'tebip' in message.content.lower():
+    if 'tebip' in message_list:
         with open('..\\img\\Akil_Sagligini_Kaybet_Tebip.png', "rb") as fh:
             f = discord.File(fh, filename='..\\img\\Akil_Sagligini_Kaybet_Tebip.png')
         await message.channel.send('Tebip mi dedin? Bak sen Tebip diyince aklıma ne geldi...')
         await message.channel.send(file=f)
 
+    # Average Nihat Berker Fan vs Nigar Berker Enjoyer
+    if 'nigar' in message_list:
+        with open('..\\img\\NigarvsNihat.mp4', "rb") as fh:
+            f = discord.File(fh, filename='..\\img\\NigarvsNihat.mp4')
+        await message.channel.send(file=f)
 
-    # trippin
-    if 'trappin' in message.content.lower():
+
+    # trappin
+    if 'trappin' in message_list:
         with open('..\\img\\trappin.png', "rb") as fh:
             f = discord.File(fh, filename='..\\img\\trappin.png')
         await message.channel.send(file=f)
 
 
     # balkan momenttt
-    if 'nagehan' in message.content.lower() or 'nagi' in message.content.lower():
-        if percent(30):
+    if 'nagehan' in message_list or 'nagi' in message.content.lower():
+        if percent(15):
             await message.channel.send('NAGEHAN MII\nBALKAN MOMENT!?!??!')
             await message.channel.send('https://www.youtube.com/watch?v=HfFx5UvzSxc')
 
@@ -80,25 +123,41 @@ async def on_message(message):
 
 
     # Köylü takla
-    if 'köylü' in message.content.lower() or 'yavuz' in message.content.lower():
-        await message.channel.send('Köylüüüü')
-        if percent(50):
-            await message.channel.send('https://youtu.be/fMimbj9Q-pU')
-        else:
-            with open('..\\img\\anime_köylü.jpg', "rb") as fh:
-                f = discord.File(fh, filename='..\\img\\anime_köylü.jpg')
-            await message.channel.send(file=f)
+    if 'köylü' in message_list or 'yavuz' in message_list:
+        if percent(35):
+            await message.channel.send('Köylüüüü')
+            if percent(50):
+                await message.channel.send('https://youtu.be/fMimbj9Q-pU')
+            else:
+                with open('..\\img\\anime_köylü.jpg', "rb") as fh:
+                    f = discord.File(fh, filename='..\\img\\anime_köylü.jpg')
+                await message.channel.send(file=f)
 
 
     # Yeterli izah yapmamışsınız. -5
     if len(str(message.content).split(' ')) < 3 and not message.content.startswith('!') and message.content != 'trappin':
-        if percent(20):
+        if percent(20) and len(message.content) < 10:
             if percent(3):
                 await message.channel.send('Yeterli izah yapmamışsınız. -10')
                 sleep(3)
                 await message.channel.send('Düzeltme:  -5, yani  -10 değil')
             else:
                 await message.channel.send('Yeterli izah yapmamışsınız. -5')
+        else:
+            pass
+
+
+    # BULLY QQN
+    if message.content.lower() == '!kanseraç':
+        tilt_etme = True
+        await message.channel.send('Kanser açık :)')
+    if message.content.lower() == '!babuşkappa':
+        tilt_etme = False
+        await message.channel.send('Kanser Kapalı :(')
+    if tilt_etme:
+        if message.author.id == 755317094937264169:
+            await message.channel.send('WOMEN MOMENT!!!!')
+            await message.channel.send('https://www.youtube.com/watch?v=e9mVfv3b-4E')
         else:
             pass
 
